23f142cece7314b1fe1374f0782ff8b93985f23d
psylon <stanislav.antunovich@yandex.ru>
2019-03-02 18:34:49 +0300

    screen for main activity (taking info to show)

---
+++ app/src/main/java/ru/psylon/weatherapp/ShowTownWeatherActivity.java
@@ -0,0 +1,13 @@
+package ru.psylon.weatherapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShowTownWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_town_weather);
+    }
+}
